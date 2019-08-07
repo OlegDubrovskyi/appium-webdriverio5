364cc8f69614287d1671f42432f089d349446da5
Administrator <admin@example.com>
2019-08-07 19:52:05 +0300

    asfasfasfasffas

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,7 +20,7 @@ exports.config = {
             automationName: 'UiAutomator2',
             appiumVersion: '1.14.1',
             platformName: 'Android',
-            platformVersion: '8.0',
+            platformVersion: '9',
             app: '/bitrise/src/app-debug.apk',
             deviceName: 'emulator-5554',
             waitforTimeout: waitforTimeout,
