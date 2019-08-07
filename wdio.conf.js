ce15a33e1d0085d42c3d78660e7e3f21a76fab89
OlegItCloud <olegdubrowsky@gmail.com>
2019-08-07 21:30:46 +0300

    agdsagsdagsd

--- wdio.conf.js
+++ wdio.conf.js
@@ -18,7 +18,7 @@ exports.config = {
     capabilities: [
         {
             automationName: 'UiAutomator2',
-            appiumVersion: '1.14.1',
+            appiumVersion: '1.15.0-fake.303',
             platformName: 'Android',
             platformVersion: '9',
             app: '/bitrise/src/app-debug.apk',
